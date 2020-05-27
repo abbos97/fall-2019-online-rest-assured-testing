bc6a39f4623f4e089a7ff7c4778061e91a99f37f
abbos97 <abbosboev@gmail.com>
2020-05-26 21:50:57 -0400

    Review practice.

--- src/test/java/com/automation/review/POJOPractice.java
+++ src/test/java/com/automation/review/POJOPractice.java
@@ -5,9 +5,10 @@ import java.util.Base64;
 public class POJOPractice {
     public static void main(String[] args) {
 
+        //We can use Base64 -to encode and decoding
             byte[] decoded = Base64.getDecoder().decode("YWRtaW46YWRtaW4=");
             String value = new String(decoded);
-            System.out.println(value);
+            System.out.println(value);// admin admin
 
     }
 }
